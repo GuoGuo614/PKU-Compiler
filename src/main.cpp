c8b0760001fad6cbfff58882ca76ec65b8aa5700
GuoGuo <2251270969@qq.com>
2025-10-26 16:35:43 +0800

    Init repo

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,5 @@
-#include <cassert>
-#include <fstream>
-#include <unordered_map>
+#include <iostream>
 
-using namespace std;
-
-std::unordered_map<char, const char *> generator = {
-    {'k', R"(fun @main(): i32 {
-%entry:
-  ret 0
-}
-)"},
-    {'r', R"(  .text
-  .globl main
-main:
-  li a0, 0
-  ret
-)"},
-    {'p', R"(  .text
-  .globl main
-main:
-  li a0, 0
-  ret
-)"},
-};
-
-int main(int argc, const char *argv[]) {
-  assert(argc == 5);
-  ofstream ofs(argv[4]);
-  ofs << generator[argv[1][1]];
-  return 0;
+int main() {
+  std::cout << "Hello, world" << std::endl;
 }
